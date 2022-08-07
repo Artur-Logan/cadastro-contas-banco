e8b3cbc58e0976e581ae167fe6cb51db1234fc9e
Artur Logan <arturlogan1ferreira@gmail.com>
2022-08-07 00:41:54 -0300

    Commit Inicial

---
+++ src/main/java/com/artur/cadastrobanco/CadastroBancoApplication.java
@@ -0,0 +1,13 @@
+package com.artur.cadastrobanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroBancoApplication.class, args);
+	}
+
+}
